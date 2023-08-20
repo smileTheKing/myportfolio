2fd6b320c2cdcb3168dbaa18a7bf7622b3e1bc33
smiletheking <s.cammue@gmail.com>
2023-08-20 16:36:11 -0500

    minor update. added link to logo

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -12,7 +12,7 @@ export default function NavigationBar({ }) {
     return (
         <div className=" p-8 bg-black flex  overflow-hidden w-full items-center sm:max-h-[80px] flex-col z-50 fixed" style={{ height: openNav ? '100vh' : '80px' }}>
             <div className="container flex sm:flex-row  gap-x-4 justify-center w-full items-center">
-                <div className=" text-white  mr-auto font-bold text-xl" >Solomon</div>
+            <Link href={'/'} className=" text-white  mr-auto font-bold text-xl" >Solomon</Link>
 
                 <ul className=" text-white hidden sm:flex  font-bold font-mono flex-row gap-y-4 sm:gap-x-4 sm:bg-inherit" >
                     <Link className=" hover:scale-90 transition  ease-lineart" href={"/"}>Home</Link>
@@ -25,9 +25,9 @@ export default function NavigationBar({ }) {
             </div>
             <div>
                 {openNav && <ul className=" text-white sm:hidden text-lg mt-8 space-y-4 h-screen w-screen justify-strart items-center flex flex-col bg-black" >
-                    <Link className=" hover:scale-90 transition  ease-lineart" href={"/project"} onClick={ handleToggle}>Project</Link>
-                    <Link className=" hover:scale-90 transition  ease-lineart" href={"/about"} onClick={ handleToggle}>About</Link>
-                    <Link className=" hover:scale-90 transition  ease-lineart" href={"/contact"} onClick={ handleToggle}>Contact</Link>
+                    <Link className=" hover:scale-90 transition  ease-lineart" href={"/project"} onClick={()=> setOpenNav(false)}>Project</Link>
+                    <Link className=" hover:scale-90 transition  ease-lineart" href={"/about"} onClick={()=> setOpenNav(false)}>About</Link>
+                    <Link className=" hover:scale-90 transition  ease-lineart" href={"/contact"} onClick={()=> setOpenNav(false)}>Contact</Link>
                 </ul>
                 }
             </div>
