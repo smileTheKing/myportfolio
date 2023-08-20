ee84686a5310204c6f1c890cc855ba3649de6edb
smiley <smiley@smileys-iMac.local>
2023-08-20 14:49:53 -0500

    added next static export config

--- next.config.js
+++ next.config.js
@@ -1,15 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode:true,
-    images: {
-        remotePatterns:[
-            {
-                protocol:'https',
-                hostname:'https://img.freepik.com',
-            port:''
-            }
-        ]
-      }
+    output: 'export',
 }
 
 
