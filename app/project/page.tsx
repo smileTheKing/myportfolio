bf8a604dd42c7ba7e3e4243385236eea914d040d
smiletheking <s.cammue@gmail.com>
2023-08-20 15:54:10 -0500

    added minor stylying parent div

--- app/project/page.tsx
+++ app/project/page.tsx
@@ -2,6 +2,10 @@ import React from 'react'
 
 export default function page() {
   return (
-    <div>page</div>
+    <main className=" min-h-screen flex flex-col items-center justify-start mb-24">
+    <div className='container  flex  flex-col gap-12  p-24'>
+    <h1>My Projects</h1>
+    </div>
+  </main>
   )
 }
