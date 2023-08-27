314cd8b06e76386856a7e4b55f4233aef68e9bf5
smiletheking <s.cammue@gmail.com>
2023-08-27 01:41:11 -0500

    minor bug updates

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
     async redirects() {
       return [
         {
-          source: '/*',
+          source: '/about',
           destination: '/',
           permanent: true,
         },
