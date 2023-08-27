329166e6d87ceae7d5b6911a39dbe651a530885f
smiletheking <s.cammue@gmail.com>
2023-08-27 02:14:09 -0500

    minor bug updates

---
+++ app/movie/page.tsx
@@ -0,0 +1,16 @@
+
+
+///import AppMovie from '@/classProjects/MovieApp/App'
+//import ContainerComponent from '@/classProjects/MovieApp/component/ContainerComponent'
+import ContainerComponent from '@/classProjects/MovieApp/component/ContainerComponent'
+import React from 'react'
+
+export default function page  (){
+  return (
+    <main className=" min-h-screen flex flex-col items-center justify-start my-24 w-full">
+       <ContainerComponent/> 
+    </main>
+ 
+  )
+}
+
